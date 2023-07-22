31e5f7e6509f24bfb615fb8bd82ff490d2385d41
Enes Bulut <139823162+enesbulut05@users.noreply.github.com>
2023-07-22 04:40:32 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -11,8 +11,8 @@ public class Main {
 	static Kart kart = new Kart();
 
 	public static void main(String[] args) throws IOException {
-		double bakiyeMiktari[] = { 500, 50, 500, 500, 500, 500, 500 }; // burasi array olmalı. Başlangıç aynı olabilir
-																		// // sonra değişecek.
+		double bakiyeMiktari[] = { 500, 500, 500, 500, 500, 500, 500 }; // burasi array olmalı. Başlangıç aynı olabilir
+																	// // sonra değişecek.
 		double bahis = 50;
 
 		boolean yeni = true; // yeni oyuna basıldığında yeni = true olup iç döngüden çıkacak.
@@ -289,68 +289,76 @@ public class Main {
 		}
 		if ((secim.equals("böl") || secim.equals("bol") || secim.equals("BOL") || secim.equals("BÖL")
 				|| secim.equals("Bol") || secim.equals("Böl"))) {
-			oyuncu.setOyuncuBoleBastiMi(true);
-			oyuncu.paraCek(oyuncu.getBahis());
-			kart.kartCek();
-			oyuncu.ekleOyuncuKartlari(kart.getCekilenKart());// index 2
-			kart.kartCek();
-			oyuncu.ekleOyuncuKartlari(kart.getCekilenKart());// index 3
-			// oyuncukarları[0,2] 1. set oyuncukarları[1,3] 2. set
-			oyuncu.ekleBolunenOyuncuKartlari1(oyuncu.getOyuncuKartlari().get(0));
-			oyuncu.ekleBolunenOyuncuKartlari1(oyuncu.getOyuncuKartlari().get(2));
-			oyuncu.ekleBolunenOyuncuKartlari2(oyuncu.getOyuncuKartlari().get(1));
-			oyuncu.ekleBolunenOyuncuKartlari2(oyuncu.getOyuncuKartlari().get(3));
-			System.out.println("Kartlar Bölündü Yeni Kartlarınız: \nSet 1 : " + oyuncu.getBolunenOyuncuKartlari1()
-					+ " Toplam Değer : " + oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari1()));
-			System.out.println("Set 2 : " + oyuncu.getBolunenOyuncuKartlari2() + " Toplam Değer : "
-					+ oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari2()));
-			for (int i = 1; i <= 2; i++) {
-				System.out.println(i + ". setiniz için seçin : PAS / KART");
-				String secim2 = scanner.nextLine();
-				if (secim2.equals("pas") || secim2.equals("PAS")) {
-					oyuncu.setBolunenSetPasaBastiMi(true, (i - 1));
-				} else if (secim2.equals("kart") || secim2.equals("KART")) {
-					kart.kartCek();
-					kart.cekilenKartiYazdir();
+			if (oyuncu.getBakiye() < oyuncu.getBahis()) {
+
+				System.out.println("Bakiyeniz Kartları Bölmek İçin Yetesiz. \nLütfen seçiniz. PAS / KART ");
+				secim = scanner.nextLine();
+			} else {
+				oyuncu.setOyuncuBoleBastiMi(true);
+				oyuncu.paraCek(oyuncu.getBahis());
+				kart.kartCek();
+				oyuncu.ekleOyuncuKartlari(kart.getCekilenKart());// index 2
+				kart.kartCek();
+				oyuncu.ekleOyuncuKartlari(kart.getCekilenKart());// index 3
+				// oyuncukarları[0,2] 1. set oyuncukarları[1,3] 2. set
+				oyuncu.ekleBolunenOyuncuKartlari1(oyuncu.getOyuncuKartlari().get(0));
+				oyuncu.ekleBolunenOyuncuKartlari1(oyuncu.getOyuncuKartlari().get(2));
+				oyuncu.ekleBolunenOyuncuKartlari2(oyuncu.getOyuncuKartlari().get(1));
+				oyuncu.ekleBolunenOyuncuKartlari2(oyuncu.getOyuncuKartlari().get(3));
+				System.out.println("Kartlar Bölündü Yeni Kartlarınız: \nSet 1 : " + oyuncu.getBolunenOyuncuKartlari1()
+						+ " Toplam Değer : " + oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari1()));
+				System.out.println("Set 2 : " + oyuncu.getBolunenOyuncuKartlari2() + " Toplam Değer : "
+						+ oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari2()));
+				for (int i = 1; i <= 2; i++) {
+					System.out.println(i + ". setiniz için seçin : PAS / KART");
+					String secim2 = scanner.nextLine();
+					if (secim2.equals("pas") || secim2.equals("PAS")) {
+						oyuncu.setBolunenSetPasaBastiMi(true, (i - 1));
+					} else if (secim2.equals("kart") || secim2.equals("KART")) {
+						kart.kartCek();
+						kart.cekilenKartiYazdir();
+						if (i == 1) {
+							oyuncu.ekleBolunenOyuncuKartlari1(kart.getCekilenKart());
+							System.out.print("1. Set : " + oyuncu.getBolunenOyuncuKartlari1());
+							System.out.print(" Toplam Değer : "
+									+ oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari1()) + "\n");
+						}
+						if (i == 2) {
+							oyuncu.ekleBolunenOyuncuKartlari2(kart.getCekilenKart());
+							System.out.print("2. Set  : " + oyuncu.getBolunenOyuncuKartlari2());
+							System.out.print(" Toplam Değer : "
+									+ oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari2()) + "\n");
+						}
+					}
 					if (i == 1) {
-						oyuncu.ekleBolunenOyuncuKartlari1(kart.getCekilenKart());
-						System.out.print("1. Set : " + oyuncu.getBolunenOyuncuKartlari1());
-						System.out.print(" Toplam Değer : "
-								+ oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari1()) + "\n");
+						try {
+							if (Integer
+									.parseInt(oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari1())) > 21) {
+								System.out.print(" PATLADIN :) \n");
+								oyuncu.setBolunenSetPasaBastiMi(true, i - 1);
+
+							}
+						} catch (Exception e) {
+
+						}
 					}
 					if (i == 2) {
-						oyuncu.ekleBolunenOyuncuKartlari2(kart.getCekilenKart());
-						System.out.print("2. Set  : " + oyuncu.getBolunenOyuncuKartlari2());
-						System.out.print(" Toplam Değer : "
-								+ oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari2()) + "\n");
-					}
-				}
-				if (i == 1) {
-					try {
-						if (Integer.parseInt(oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari1())) > 21) {
-							System.out.print(" PATLADIN :) \n");
-							oyuncu.setBolunenSetPasaBastiMi(true, i - 1);
+						try {
+							if (Integer
+									.parseInt(oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari2())) > 21) {
+								System.out.print(" PATLADIN :) \n");
+								oyuncu.setBolunenSetPasaBastiMi(true, i - 1);
+
+							}
+						} catch (Exception e) {
 
 						}
-					} catch (Exception e) {
-
-					}
-				}
-				if (i == 2) {
-					try {
-						if (Integer.parseInt(oyuncu.oyuncuSetleriniHesapla(oyuncu.getBolunenOyuncuKartlari2())) > 21) {
-							System.out.print(" PATLADIN :) \n");
-							oyuncu.setBolunenSetPasaBastiMi(true, i - 1);
-
-						}
-					} catch (Exception e) {
-
 					}
 				}
 			}
 		}
 
-		else if (secim.equals("kart") || secim.equals("KART")) {
+		if (secim.equals("kart") || secim.equals("KART")) {
 			kart.kartCek();
 			kart.cekilenKartiYazdir();
 			oyuncu.ekleOyuncuKartlari(kart.getCekilenKart());
